55651419dd9d683906802a32cbc252e84db8515c
Noah Kattner <gasdrop14@gmail.com>
2025-05-07 15:41:02 -0400

    Better code organization

--- Lab16_nkattner-1.py
+++ Lab16_nkattner-1.py
@@ -8,32 +8,39 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+class Main:
 
-path = Path('OHUR.csv')
-lines = path.read_text(encoding='utf-8').splitlines()
+    def __init__(self):
+        
+        self.path = Path('OHUR.csv')
+        self.lines = self.path.read_text(encoding='utf-8').splitlines()
 
-reader = csv.reader(lines)
-header_row = next(reader)
+        self.reader = csv.reader(self.lines)
+        self.header_row = next(self.reader)
 
-# for index, col_title in enumerate(header_row):
-#     print(f'{index} {col_title}, ', end=' ')
-# print()
+        self.dates = []
+        self.unemployment_rates = []
 
-dates = []
-unemployment_rates = []
+        self.graph_data()
 
-for row in reader:
-    current_date = datetime.strptime(row[0], '%Y-%m-%d')
-    unrate = float(row[1])
+    def graph_data(self):
+        self.get_data()
+        plt.style.use('grayscale')
+        figure, graph = plt.subplots()
+        graph.plot(self.dates, self.unemployment_rates, color='blue')
 
-    dates.append(current_date)
-    unemployment_rates.append(unrate)
+        graph.set_title('Unemployment Rates in Ohio since 1976', fontsize=14)
+        graph.set_xlabel("Year", fontsize=14)
+        graph.set_ylabel('Unemp Rate', fontsize=14)
+        plt.show()
 
-plt.style.use('grayscale')
-figure, graph = plt.subplots()
-graph.plot(dates, unemployment_rates, color='blue')
+    def get_data(self):
+        for row in self.reader:
+            current_date = datetime.strptime(row[0], '%Y-%m-%d')
+            unrate = float(row[1])
 
-graph.set_title('Unemployment Rates in Ohio since 1976', fontsize=14)
-graph.set_xlabel("Year", fontsize=14)
-graph.set_ylabel('Unemp Rate', fontsize=14)
-plt.show()
+            self.dates.append(current_date)
+            self.unemployment_rates.append(unrate)
+
+if __name__ == '__main__':
+    Main()
